42d6fc5651bebc57d580de01166f5e2fe3853bff
tantuan003 <tantuan0162@gmail.com>
2025-11-05 18:33:14 +0700

    tao bang workspace

--- src/models/UserModel.js
+++ src/models/UserModel.js
@@ -4,6 +4,7 @@ const UserSchema = new mongoose.Schema({
   username: { type: String, required: true },
   email:    { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  workspaces: [{ type: mongoose.Schema.Types.ObjectId, ref: "Workspace" }],
 }, { timestamps: true });
 
 export default mongoose.model("User", UserSchema);
