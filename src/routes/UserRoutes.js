3ee948d1e4c16aa53dbc3d6d02c49e7b24b3db80
tantuan003 <tantuan0162@gmail.com>
2025-11-05 17:56:35 +0700

    hoan thanh logic dang nhap

--- src/routes/UserRoutes.js
+++ src/routes/UserRoutes.js
@@ -1,9 +1,11 @@
 import express from "express";
 import { registerUser } from "../controllers/UserController.js";
 import { CreateUser_validation } from "../middlewares/Validation.js";
+import { loginUser } from "../controllers/AuthController.js";
 
 const router = express.Router();
 
 router.post("/register", CreateUser_validation, registerUser);
+router.post("/login", loginUser);
 
 export default router;
