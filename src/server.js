3a90f7fc9223c1b021c272aafc0744172d176b04
tantuan003 <tantuan0162@gmail.com>
2025-11-05 21:26:10 +0700

    chinh sua logic register va login

--- src/server.js
+++ src/server.js
@@ -16,6 +16,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
+app.use(express.urlencoded({ extended: true })); // để nhận form dữ liệu
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: { origin: "*" } // Cho phép frontend truy cập
